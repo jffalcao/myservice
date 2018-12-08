dcd5410a837982149e3ab1ef3093786271284553
Jose Fernando Falcao <thegardenautomaton@gmail.com>
2018-12-08 10:20:27 -0500

    Verification Message Changed

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-// var express = require('express');
-// var app = express();
-
-// app.get('/', (req, res) => {
-//     res.json({'msg': 'Hello World'});
-// });
-
-// var port = process.env.port || 3000;
-
-// app.listen(port, () => {
-//     console.log('Server Started at ' + port);
-// });
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.json({'msg': 'Hello World Again!'});
+    res.json({'msg': 'myservice is up and running correctly!'});
 });
 
 var port = process.env.port || 3000;
